c157790830ae5ef8dc981ad4cdc61d7c2e949e25
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 17:29:53 +0600

    mongodb send data sunccesfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -26,12 +26,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const chocolateCollection = client.db('chocolateDB').collection('chocolates') ;
+
+    app.get('/chocolates',async(req,res)=>{
+        const cursor = chocolateCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // app.get('/chocolates/:id',async(req,res)=>{
+    //     const id = req.params.id;
+    //     const query = {_id: new ObjectId(id)} ;
+    //     const result = await chocolateCollection.findOne(query);
+    //     res.send(result);
+    // })
+
+    app.post('/chocolates',async(req,res)=>{
+        const chocolate = req.body;
+        // console.log(chocolate);
+        const result = await chocolateCollection.insertOne(chocolate);
+        res.send(result);
+    })
+
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.log);
