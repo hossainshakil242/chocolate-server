149602008aee3045c3710774f602967a803f118a
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 15:02:09 +0600

    server site conected successfully with mongobd. and apply secrate key

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
+
+// MongoDB Conected
+
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.vwvxcgj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.log);
+
 
 app.get('/',(req,res)=>{
     res.send('This is My Chocolate Server')
