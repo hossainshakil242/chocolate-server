d404facad5b366425458f4abd8ed9a7f9b279f92
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 18:14:52 +0600

    deleted success fully

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/chocolates/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await chocolateCollection.deleteOne(query);
+        res.send(result);
+    })
+
     
 
     // Send a ping to confirm a successful connection
